69e15fc9681caf9d67510ab07305e746d08f042a
Mosharraf Musa <mosharrafny@gmail.com>
2020-07-14 20:18:56 -0400

    Passed 1st Test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,19 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("enhancers module", () => {
+  describe("repair function", () => {
+    it("repairs the item", () => {
+      const expectedOutput = {
+        name: "sword",
+        durability: 100,
+        enhancement: 10,
+      };
+      const actualOutput = enhancer.repair({
+        name: "sword",
+        durability: 9,
+        enhancement: 10,
+      });
+      expect(actualOutput).toMatchObject(expectedOutput);
+    });
+  });
+});
